310d102a83dc5eed0a643a8639df14c420643da8
Viktoria Husaruk <vhusaruk92@gmail.com>
2020-04-02 12:43:14 +0300

    [TASK] changes in template, add swiper library, js

--- theme/src/js/main.js
+++ theme/src/js/main.js
@@ -1,3 +1,6 @@
 import './main/contentElements/content1'
 import './main/contentElements/content2'
 import './main/header/header'
+
+import '../../../node_modules/swiper/js/swiper.min.js'
+import './main/contentElements/slider'
